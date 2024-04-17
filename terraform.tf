fea67e58e8251e9c33a10704cd0c0ae9b02d6bc6
chowdarybvsn <chowdarybvsn@gmail.com>
2024-04-18 00:23:20 +0530

    1st commit

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+   bucket = "terraform-project-1-2024"
+   key = "state/terraform.tfstate"
+   region = "us-east-1"
+   encrypt = true
+   dynamodb_table = "2024"
+  }
+}
+
+
